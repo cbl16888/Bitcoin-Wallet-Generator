62f28e3d1167e7f6d9625f0598f296294a442840
yehan-wasura <61929215+yehan-wasura@users.noreply.github.com>
2020-09-28 18:27:55 +0530

    Update bruteforcer.py

--- bruteforcer.py
+++ bruteforcer.py
@@ -2,7 +2,7 @@ import os
 range1 = int(input("Input Starting Range :> "))
 range2 = int(input("Input Ending Range   :> "))
 
-for x in range(range1,rang2):
+for x in range(range1,range2):
 	print(str(x))
 	os.system(r'python wallet_generator.py '+ str(x))
 	x = x + 1
